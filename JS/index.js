d428eb57170ba3c0d5ce8eb88ec9f0c53b95eef2
krystel <krystel.dev@gmail.com>
2022-11-24 16:58:18 +0100

    fetch datos proyecto selected

--- JS/index.js
+++ JS/index.js
@@ -44,18 +44,31 @@ if (typeof CTAForm != "undefined") {
 }
 
 /* ********************** Start Project (selected)********************** */
-// prueba para rellenar un objeto a la api
+
+// ver que hay antes de rellenar:
+//Listing all resources
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+// Enviamos los datos a la API y entonces
 fetch("https://jsonplaceholder.typicode.com/posts", {
   method: "POST",
   body: JSON.stringify({
-    title: "foo",
-    body: "bar",
-    userId: 1,
+    title: "Simplify",
+    body: "Simplify is lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat faucibus sem, id vulputate justo ornare non. Nullam et vehicula sem. Pellentesque bibendum massa non nisl posuere cursus. Aliquam ac eros pretium neque consequat facilisis. Ut sit amet justo varius ex ultrices fringilla. Sed vel turpis fringilla ante gravida vestibulum. Etiam a sollicitudin leo. Nunc metus lectus, lacinia ut arcu quis, pretium congue lacus.",
+    userId:
+      "Aliquam blandit varius metus, in mattis orci convallis sit amet. Fusce et sem ac velit placerat ultrices in id dolor. Sed eget auctor metus. Sed suscipit fringilla lectus, a maximus justo bibendum a. Donec et pellentesque nisl, vitae tempus lorem. Sed efficitur cursus rhoncus. Duis placerat nisi quis mi tincidunt, sit amet tincidunt urna convallis. Nam et sagittis felis. Pellentesque placerat orci sodales nisi convallis ultrices. Nunc posuere neque eu mi imperdiet ullamcorper. Praesent tempor convallis rutrum. Vestibulum dignissim pellentesque eros in placerat. Etiam at nisi quis quam sollicitudin vehicula sit amet vel mi.",
   }),
   headers: {
     "Content-type": "application/json; charset=UTF-8",
   },
 })
   .then((response) => response.json())
-  .then((json) => console.log(json))
+  .then((json) => {
+    proyect = json;
+    document.querySelector("#titleProyect").innerHTML += json.title;
+    document.querySelector("#bodyProyect").innerHTML += json.body;
+    document.querySelector("#userIdProyect").innerHTML += json.userId;
+  })
   .catch((error) => console.log(error));
